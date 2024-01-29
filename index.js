cbe59f8cde2660ba69f32c20d98ab2fc8c204eb2
cbwyash <100893735+cbwyash@users.noreply.github.com>
2024-01-28 22:55:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [438100, 1049590, 1599340, 237310, 774171, 2225070, 1056640, 1343400, 306130];  // Enter here AppIDs of the needed games
+var games = [438100, 1049590, 1599340, 237310, 774171, 2225070, 1056640, 1343400, 306130, 771580];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 
 
